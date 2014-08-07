37e3d0dd1fa1a8bd64d85ffd0658498ffcd8c8eb
NitinRGupta <nitinramachandragupta@gmail.com>
2014-08-07 12:43:37 +0530

    files

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,354 @@
+﻿
+namespace Microsoft.Samples.Kinect.DepthBasics
+{
+    using System;
+    using System.Globalization;
+    using System.IO;
+    using System.Windows;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using Microsoft.Kinect;
+    using System.Windows.Forms;
+    using System.Speech.Synthesis;
+    using System.Speech.AudioFormat;
+
+
+    public static class Globals
+    {
+        public static string value = "1"; // Modifiable in Code"
+        //public const Int32 VALUE = 10; // Unmodifiable
+    }
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        /// <summary>
+        /// Active Kinect sensor
+        /// </summary>
+        public KinectSensor sensor;
+
+        /// <summary>
+        /// Bitmap that will hold color information
+        /// </summary>
+        public WriteableBitmap colorBitmap;
+
+        /// <summary>
+        /// Intermediate storage for the depth data received from the camera
+        /// </summary>
+        public DepthImagePixel[] depthPixels;
+
+        /// <summary>
+        /// Intermediate storage for the depth data converted to color
+        /// </summary>
+        public byte[] colorPixels;
+
+        /// <summary>
+        /// Initializes a new instance of the MainWindow class.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            //display();
+        }
+
+        /// <summary>
+        /// Execute startup tasks
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        public void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            // Look through all sensors and start the first connected one.
+            // This requires that a Kinect is connected at the time of app startup.
+            // To make your app robust against plug/unplug, 
+            // it is recommended to use KinectSensorChooser provided in Microsoft.Kinect.Toolkit (See components in Toolkit Browser).
+            foreach (var potentialSensor in KinectSensor.KinectSensors)
+            {
+                if (potentialSensor.Status == KinectStatus.Connected)
+                {
+                    this.sensor = potentialSensor;
+
+                    break;
+                }
+            }
+
+            if (null != this.sensor)
+            {
+                // Turn on the depth stream to receive depth frames
+                this.sensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+
+                // Allocate space to put the depth pixels we'll receive
+                this.depthPixels = new DepthImagePixel[this.sensor.DepthStream.FramePixelDataLength];
+
+                // Allocate space to put the color pixels we'll create
+                this.colorPixels = new byte[this.sensor.DepthStream.FramePixelDataLength * sizeof(int)];
+
+                // This is the bitmap we'll display on-screen
+                this.colorBitmap = new WriteableBitmap(this.sensor.DepthStream.FrameWidth, this.sensor.DepthStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
+
+                // Set the image we display to point to the bitmap where we'll put the image data
+                this.Image.Source = this.colorBitmap;
+
+                // Add an event handler to be called whenever there is new depth frame data
+                //this.sensor.DepthFrameReady += this.SensorDepthFrameReady;
+
+
+                // Start the sensor!
+                try
+                {
+                    this.sensor.Start();
+
+                }
+                catch (IOException)
+                {
+                    this.sensor = null;
+                }
+            }
+
+            if (null == this.sensor)
+            {
+                this.statusBarText.Text = Properties.Resources.NoKinectReady;
+            }
+
+        }
+
+        /// <summary>
+        /// Execute shutdown tasks
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        public void WindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+            if (null != this.sensor)
+            {
+                this.sensor.Stop();
+            }
+        }
+
+        /// <summary>
+        /// Event handler for Kinect sensor's DepthFrameReady event
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+
+
+
+        public void SensorDepthFrameReady(object sender, DepthImageFrameReadyEventArgs e)
+        {
+
+            using (DepthImageFrame depthFrame = e.OpenDepthImageFrame())
+            {
+
+                if (depthFrame != null)
+                {
+
+                    // Copy the pixel data from the image to a temporary array
+                    depthFrame.CopyDepthImagePixelDataTo(this.depthPixels);
+
+                    // Get the min and max reliable depth for the current frame
+                    // int minDepth = depthFrame.MinDepth;
+                    //int maxDepth = depthFrame.MaxDepth;
+
+                    // Convert the depth to RGB
+                    int colorPixelIndex = 0;
+                    // for (; ; )
+                    //{
+                    for (int i = 0; i < this.depthPixels.Length; ++i)
+                    {
+
+                        // Get the depth for this pixel 
+
+                        short depth = depthPixels[i].Depth;
+                        // To convert to a byte, we're discarding the most-significant
+                        // rather than least-significant bits.
+                        // We're preserving detail, although the intensity will "wrap."
+                        // Values outside the reliable depth range are mapped to 0 (black).
+
+                        // Note: Using conditionals in this loop could degrade performance.
+                        // Consider using a lookup table instead when writing production code.
+                        // See the KinectDepthViewer class used by the KinectExplorer sample
+                        // for a lookup table example.
+                        byte intensity = (byte)(depth <= 1000 ? depth : 0);
+
+                        // Write out blue byte
+                        this.colorPixels[colorPixelIndex++] = intensity;
+
+                        // Write out green byte
+                        this.colorPixels[colorPixelIndex++] = intensity;
+
+                        // Write out red byte                        
+                        this.colorPixels[colorPixelIndex++] = intensity;
+
+                        // We're outputting BGR, the last byte in the 32 bits is unused so skip it
+                        // If we were outputting BGRA, we would write alpha here.
+
+                        if (this.colorPixels[colorPixelIndex] != intensity)
+                        {
+                            Globals.value = "0";
+                        }
+                        ++colorPixelIndex;
+                        //System.Windows.MessageBox.Show("Stahp");
+                        //display(depth, minDepth);
+
+
+                    }
+
+
+                    // }
+
+                    // Write the pixel data into our bitmap
+                    this.colorBitmap.WritePixels(
+                        new Int32Rect(0, 0, this.colorBitmap.PixelWidth, this.colorBitmap.PixelHeight),
+                        this.colorPixels,
+                        this.colorBitmap.PixelWidth * sizeof(int), 0);
+
+
+                }
+
+            }
+
+        }
+        /*public void display()
+        {
+               MessageBox.Show("stop");
+        }*/
+
+
+
+        /* public void display(short depthData, int min)
+          {
+              MessageBox.Show("Stahp");
+              //initialize a StreamWriter
+             // StreamWriter sw = new StreamWriter(@"C:/Example.txt");
+
+              //search the depth data and add it to the file
+              //for (int i = 0; i < depthData.Length; i++)
+              //{
+                 // sw.WriteLine(depthData[i] + "\n"); //\n for a new line
+              //}
+
+              //dispose of sw
+              //sw.Close();
+              if (depthData <= min)
+              {
+                  MessageBox.Show("Stahp");
+              }
+           }*/
+
+
+        /// <summary>
+        /// Handles the user clicking on the screenshot button
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void ButtonScreenshotClick(object sender, RoutedEventArgs e)
+        {
+            if (null == this.sensor)
+            {
+                this.statusBarText.Text = Properties.Resources.ConnectDeviceFirst;
+                return;
+            }
+            System.Windows.Forms.MessageBox.Show("Screenshot Taken");
+            // create a png bitmap encoder which knows how to save a .png file
+            BitmapEncoder encoder = new PngBitmapEncoder();
+
+            // create frame from the writable bitmap and add to encoder
+            encoder.Frames.Add(BitmapFrame.Create(this.colorBitmap));
+
+            string time = System.DateTime.Now.ToString("hh'-'mm'-'ss", CultureInfo.CurrentUICulture.DateTimeFormat);
+
+            string myPhotos = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+            string path = Path.Combine(myPhotos, "KinectSnapshot-" + time + ".png");
+
+            // write the new file to disk
+            try
+            {
+                using (FileStream fs = new FileStream(path, FileMode.Create))
+                {
+                    encoder.Save(fs);
+                }
+
+                this.statusBarText.Text = string.Format("{0} {1}", Properties.Resources.ScreenshotWriteSuccess, path);
+            }
+            catch (IOException)
+            {
+                this.statusBarText.Text = string.Format("{0} {1}", Properties.Resources.ScreenshotWriteFailed, path);
+            }
+        }
+
+        /// <summary>
+        /// Handles the checking or unchecking of the near mode combo box
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void CheckBoxNearModeChanged(object sender, RoutedEventArgs e)
+        {
+            if (this.sensor != null)
+            {
+                // will not function on non-Kinect for Windows devices
+
+                try
+                {
+                    if (this.checkBoxNearMode.IsChecked.GetValueOrDefault())
+                    {
+                        this.sensor.DepthStream.Range = DepthRange.Near;
+                    }
+                    else
+                    {
+                        this.sensor.DepthStream.Range = DepthRange.Default;
+                    }
+                }
+                catch (InvalidOperationException)
+                {
+                }
+
+            }
+        }
+
+
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+            this.sensor.DepthFrameReady += this.SensorDepthFrameReady;
+            if (Globals.value == "0")
+            {
+
+                // Initialize a new instance of the speech synthesizer.
+                using (SpeechSynthesizer synth = new SpeechSynthesizer())
+                using (MemoryStream stream = new MemoryStream())
+                {
+
+                    // Create a SoundPlayer instance to play the output audio file.
+                    //MemoryStream streamAudio = new MemoryStream();
+                    System.Media.SoundPlayer m_SoundPlayer = new System.Media.SoundPlayer();
+
+                    // Configure the synthesizer to output to an audio stream.
+                    synth.SetOutputToWaveStream(streamAudio);
+
+                    // Speak a phrase.
+                    synth.Speak("stop!! obstacle detected.");
+                    
+                    streamAudio.Position = 0;
+                    m_SoundPlayer.Stream = streamAudio;
+                    m_SoundPlayer.Play();
+
+                    // Set the synthesizer output to null to release the stream. 
+                    synth.SetOutputToNull();
+                    System.Windows.Forms.MessageBox.Show("press ok");
+                    Globals.value = "1";
+                    // Insert code to persist or process the stream contents here.
+                }
+
+               
+            }
+        }
+
+        
+    }
+
+}// 
